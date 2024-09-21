987cf966cdfa6061ca65c6eccccf03724e98cc9c
Rolando <rol4ndo99@gmail.com>
2024-09-21 19:49:02 -0400

    added metadata

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Mesas } from './components/mesas/Mesas';
 import { AdminScreen, Cocina, Menu, Mesero } from './screens';
 import { useSocketStore } from './store/useSocketStore';
 import { MeseroContainer } from './views/meseroView/MeseroContainer';
+import { Helmet } from 'react-helmet';
 
 function App() {
 
@@ -21,17 +22,24 @@ function App() {
 
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/mesero" element={<Mesero />} />
-        <Route path="/cocina" element={<Cocina />} />
-        <Route path="/mesas" element={<Mesas />} />
-        <Route path="/mesa/:id" element={<MeseroContainer />} />
-        <Route path="/admin" element={<AdminScreen />} />
-        <Route path="*" element={<Navigate to="/menu" />} />
-      </Routes>
-    </Router>
+    <>
+      <Helmet>
+        <title>Mi Página de Inicio</title>
+        <meta name="description" content="Esta es la página de inicio de mi aplicación" />
+        <meta name="keywords" content="React, TypeScript, Material-UI" />
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/mesero" element={<Mesero />} />
+          <Route path="/cocina" element={<Cocina />} />
+          <Route path="/mesas" element={<Mesas />} />
+          <Route path="/mesa/:id" element={<MeseroContainer />} />
+          <Route path="/admin" element={<AdminScreen />} />
+          <Route path="*" element={<Navigate to="/menu" />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
