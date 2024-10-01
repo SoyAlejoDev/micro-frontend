f97406daff305abc6e9b53210e5814de31c8568e
Rolando <rol4ndo99@gmail.com>
2024-10-01 17:00:54 -0400

    fix RenderContent

--- src/views/menuView/RenderContent.tsx
+++ src/views/menuView/RenderContent.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Data } from '../../types';
 import { Characteristics } from './characteristics/Characteristics';
 import { Footer } from './Footer';
@@ -11,37 +12,18 @@ interface Props {
     seccionesMenu: string[];
 }
 
-export const RenderContent = ({ selectedMenu, socketData, seccionesMenu }: Props) => {
-    switch (selectedMenu) {
-        case seccionesMenu[0]:
-            return (
-                <>
-                    <MainMenu socketData={socketData} />
-                    <History socketData={socketData} />
-                    <Characteristics socketData={socketData} />
-                    <Footer socketData={socketData} />
-                </>
-            );
-        case seccionesMenu[1]:
-            return <MenuItem seccion={seccionesMenu[1]} />;
-        case seccionesMenu[2]:
-            return <MenuItem seccion={seccionesMenu[2]} />;
-        case seccionesMenu[3]:
-            return <MenuItem seccion={seccionesMenu[3]} />;
-        case seccionesMenu[4]:
-            return <MenuItem seccion={seccionesMenu[4]} />;
-        case seccionesMenu[5]:
-            return <MenuItem seccion={seccionesMenu[5]} />;
-        case seccionesMenu[6]:
-            return <MenuItem seccion={seccionesMenu[6]} />;
-        default:
-            return (
-                <>
-                    <MainMenu socketData={socketData} />
-                    <History socketData={socketData} />
-                    <Characteristics socketData={socketData} />
-                    <Footer socketData={socketData} />
-                </>
-            );
+export const RenderContent: React.FC<Props> = ({ selectedMenu, socketData, seccionesMenu }) => {
+    if (selectedMenu === seccionesMenu[0] || !seccionesMenu.includes(selectedMenu)) {
+        return (
+            <>
+                <MainMenu socketData={socketData} />
+                <History socketData={socketData} />
+                <Characteristics socketData={socketData} />
+                <Footer socketData={socketData} />
+            </>
+        );
     }
+
+    const index = seccionesMenu.indexOf(selectedMenu);
+    return <MenuItem seccion={seccionesMenu[index]} />;
 };
